4ed1663bae1864be01ab0a4795b41429548cdbf2
Adrian Valenzuela <avalenzuela85@gmail.com>
2021-12-19 12:48:35 -0800

    fixed tests for sorcery auth

--- test/models/listing_test.rb
+++ test/models/listing_test.rb
@@ -1,10 +1,8 @@
 require "test_helper"
 
 class ListingTest < ActiveSupport::TestCase
-  def setup
-    @listing = listings(:one)
-  end
   # test "the truth" do
   #   assert true
   # end
+  #
 end
